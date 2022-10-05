6b01fe2f8fbb5c242e14d6bfb8d1a21460681efd
andreipetrescu <andrei.petrescu@cloudhero.io>
2022-10-05 10:44:57 +0300

    fix node group attach role

--- terraform/modules/eks_node_group/iam_attach.tf
+++ terraform/modules/eks_node_group/iam_attach.tf
@@ -5,7 +5,7 @@ resource "aws_iam_role_policy_attachment" "nodes_policy" {
 
 resource "aws_iam_role_policy_attachment" "ecr_policy" {
   role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_iam_role_policy_attachment" "cni_policy" {
