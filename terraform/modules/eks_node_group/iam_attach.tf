d763695412b9dc6b0924e78b0323ac6255d66c8c
andreipetrescu <andrei.petrescu@cloudhero.io>
2024-03-26 21:09:26 +0200

    attach ebs policy to nodes

--- terraform/modules/eks_node_group/iam_attach.tf
+++ terraform/modules/eks_node_group/iam_attach.tf
@@ -12,3 +12,8 @@ resource "aws_iam_role_policy_attachment" "cni_policy" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
+
+resource "aws_iam_role_policy_attachment" "ebs_policy" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
